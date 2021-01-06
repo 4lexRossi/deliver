3bd879d0b719ba6063a71a1ee1c659049db58d76
4lexRossi <devalexrossi@gmail.com>
2021-01-06 12:45:09 -0300

    GET /orders

--- backend/src/main/java/com/alos/deliver/repositories/OrderRepository2.java
+++
@@ -1,9 +0,0 @@
-package com.alos.deliver.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.alos.deliver.entities.Order;
-
-public interface OrderRepository2 extends JpaRepository<Order, Long>{
-	
-}
